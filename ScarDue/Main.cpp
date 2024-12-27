3d1a681834eb892d8e6141bd98ce07f73f78d8bd
osama2alashry <66707628+osama2alashry@users.noreply.github.com>
2024-12-27 16:38:01 +0200

    Update Main.cpp

--- ScarDue/Main.cpp
+++ ScarDue/Main.cpp
@@ -60,8 +60,8 @@ int main()
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "ScarDue"
+	GLFWwindow* window = glfwCreateWindow(width, height, "ScarDue", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
